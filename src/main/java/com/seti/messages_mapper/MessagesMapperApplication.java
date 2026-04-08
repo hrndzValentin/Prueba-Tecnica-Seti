6339fb5e6a46851e1695f1911850dc040f756987
Valentin Sanchez <vaher.246@gmail.com>
2026-04-08 16:15:00 -0500

    project setup

---
+++ src/main/java/com/seti/messages_mapper/MessagesMapperApplication.java
@@ -0,0 +1,13 @@
+package com.seti.messages_mapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagesMapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagesMapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seti/messages_mapper/MessagesMapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seti.messages_mapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagesMapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
